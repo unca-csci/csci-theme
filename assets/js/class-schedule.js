9602ce63837ac700d800f40fc714c53b28489d51
Sarah <vanwars@gmail.com>
2023-10-11 06:42:17 -0400

    Adding Spring 2024

--- assets/js/class-schedule.js
+++ assets/js/class-schedule.js
@@ -1,18 +1,24 @@
-import DataManager from './data-manager.js';
-import utils from './utilities.js';
+import DataManager from "./data-manager.js";
+import utils from "./utilities.js";
 
 export default class ClassSchedule {
-    
-    constructor () {
-        document.querySelector('#unca-classes').innerHTML = this.getInterfaceHTML();
-        utils.showSpinner(document.querySelector('#course-list'), 'Loading the university course catalog...');
-        document.querySelector('#term').addEventListener('change', this.showNewTerm.bind(this));
+    constructor() {
+        document.querySelector("#unca-classes").innerHTML =
+            this.getInterfaceHTML();
+        utils.showSpinner(
+            document.querySelector("#course-list"),
+            "Loading the university course catalog..."
+        );
+        document
+            .querySelector("#term")
+            .addEventListener("change", this.showNewTerm.bind(this));
         this.dm = window.dataManager = new DataManager();
     }
 
     getInterfaceHTML() {
         return `
         <select id="term">
+            <option value="2024/spring/">Spring 2024</option>
             <option value="2023/fall/">Fall 2023</option>
             <option value="2023/spring/">Spring 2023</option>
             <option value="2022/fall/">Fall 2022</option>
@@ -26,18 +32,17 @@ export default class ClassSchedule {
         `;
     }
 
-
-    async fetchAndDisplay () {
-        const term = document.querySelector('#term').value;
+    async fetchAndDisplay() {
+        const term = document.querySelector("#term").value;
         await this.dm.initializeCourseScheduleData(term);
-        const parent = document.querySelector('#course-list');
+        const parent = document.querySelector("#course-list");
         this.displayClasses(parent);
     }
 
     displayClasses(parent) {
-        parent.innerHTML = '';
-        this.dm.classes.forEach(uncaClass => {
-            parent.insertAdjacentHTML('beforeend', uncaClass.getTemplate());
+        parent.innerHTML = "";
+        this.dm.classes.forEach((uncaClass) => {
+            parent.insertAdjacentHTML("beforeend", uncaClass.getTemplate());
             const classEl = parent.lastElementChild;
             uncaClass.appendTitle(classEl);
             uncaClass.appendInstructor(classEl);
@@ -45,14 +50,13 @@ export default class ClassSchedule {
     }
 
     async showNewTerm() {
-        const parent = document.querySelector('#course-list');
-        utils.showSpinner(parent, 'Loading the university course catalog...');
-        
-        const term = document.querySelector('#term').value;
+        const parent = document.querySelector("#course-list");
+        utils.showSpinner(parent, "Loading the university course catalog...");
+
+        const term = document.querySelector("#term").value;
         await this.dm.updateClassesByTerm(term);
         this.displayClasses(parent);
     }
-
 }
 
 const classSchedule = new ClassSchedule();
