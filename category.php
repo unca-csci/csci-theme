4f288086862cd338820ba929e86e7f7c93a65c90
Sarah <vanwars@gmail.com>
2024-03-17 16:22:49 -0400

    tests

--- category.php
+++ category.php
@@ -12,7 +12,10 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		categories
+    <?php
+        the_archive_title( '<h1 class="page-title">', '</h1>' );
+        the_archive_description( '<div class="archive-description">', '</div>' );
+    ?>
 
 	</main><!-- #main -->
 
