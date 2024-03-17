01949220f1adb0d5d902af25285eefdbcdb691fd
Sarah <vanwars@gmail.com>
2024-03-17 17:15:50 -0400

    added Post model

--- assets/js/posts-by-category.js
+++ assets/js/posts-by-category.js
@@ -13,13 +13,10 @@ export default class PostsByCategory {
         const posts = await this.dm.getPostsByCategory(this.categoryId);
         console.log(posts);
         this.mainContainer.innerHTML = '<div class="projects"></div>';
-        const container = this.parent.lastElementChild;
+        const container = this.mainContainer.lastElementChild;
         posts.forEach((post) => {
             console.log(post.name, post.term);
-            container.insertAdjacentHTML(
-                "beforeend",
-                post.getCardTemplate()
-            );
+            container.insertAdjacentHTML("beforeend", post.getCardTemplate());
         });
     }
 }
