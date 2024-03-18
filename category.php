16999d279b7de4fa7df66d0dba6c4653b9fc5761
Sarah <vanwars@gmail.com>
2024-03-18 10:31:39 -0400

    formatting

--- category.php
+++ category.php
@@ -12,20 +12,21 @@ get_header();
 
 	<main id="primary" class="site-main categories">
 
-    <?php
-        echo  '<h1 class="news-h1">' . single_cat_title("",false) . '</h1>';
-        $cur_cat = get_cat_ID( single_cat_title("",false) );
-        $js_path = get_stylesheet_directory_uri();
-    ?>
+        <?php
+            echo  '<h1 class="news-h1">' . single_cat_title("",false) . '</h1>';
+            $cur_cat = get_cat_ID( single_cat_title("",false) );
+            $js_path = get_stylesheet_directory_uri();
+        ?>
 
-    <script type="module">
-        import PostsByCategory from "<?php echo $js_path ?>/assets/js/posts-by-category.js";
+        <script type="module">
+            import PostsByCategory from "<?php echo $js_path ?>/assets/js/posts-by-category.js";
 
-        const posts = new PostsByCategory(<?php echo $cur_cat ?>);
-        posts.fetchAndDisplayByCategory();
+            const posts = new PostsByCategory(<?php echo $cur_cat ?>);
+            posts.fetchAndDisplayByCategory();
 
-    </script>
-    <div id="category-main">Categories go here...</div>
+        </script>
+        
+        <div id="category-main"></div>
 
 	</main><!-- #main -->
 
