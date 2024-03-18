ac1b15f0100b6951022c2ac0641cdaaf7913e270
Sarah <vanwars@gmail.com>
2024-03-18 10:17:00 -0400

    formatting

--- category.php
+++ category.php
@@ -13,7 +13,7 @@ get_header();
 	<main id="primary" class="site-main categories">
 
     <?php
-        the_archive_title( '<h1 class="page-title">', '</h1>' );
+        the_archive_title( '<h1 class="news-h1">', '</h1>' );
         $cur_cat = get_cat_ID( single_cat_title("",false) );
         $js_path = get_stylesheet_directory_uri();
     ?>
