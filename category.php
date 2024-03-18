6a5ea3f73561ac1cd90f51bad30d8963e8f214f6
Sarah <vanwars@gmail.com>
2024-03-17 20:29:02 -0400

    added Post model

--- category.php
+++ category.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main categories">
 
     <?php
         the_archive_title( '<h1 class="page-title">', '</h1>' );
