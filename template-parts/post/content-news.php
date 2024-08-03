dae20ec610c2a1a24ce18f3ef8a0239116de7ba7
Sarah <vanwars@gmail.com>
2024-08-03 14:46:35 -0400

    CSS Updates

--- template-parts/post/content-news.php
+++ template-parts/post/content-news.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     
-    <div class="entry-wrapper two-column">
+    <div class="entry-wrapper two-column news">
         <div class="entry-content">
             <header class="entry-header">
                 <?php
